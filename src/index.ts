b767af315ef8fcf0e41311cffaac005013ed0c12
BoussonKarel <bousson.karel@gmail.com>
2023-06-27 22:05:16 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,41 @@
 const express = require("express");
-import { AppDataSource } from "./data-source"
+import { AppDataSource } from "./data-source";
 import { Product } from "./entity/Product";
-import { Supplier } from "./entity/Supplier"
+import { Supplier } from "./entity/Supplier";
 
-AppDataSource.initialize().then(async () => {
+// CORS
+const cors = require("cors");
 
+AppDataSource.initialize()
+  .then(async () => {
     // console.log("Inserting a new supplier into the database...")
     // const supplier = new Supplier()
     // supplier.name = "Chipskey"
     // await AppDataSource.manager.save(supplier)
     // console.log("Saved a new supplier with id: " + supplier.id)
 
-    console.log("Loading suppliers from the database...")
-    const suppliers = await AppDataSource.manager.find(Supplier)
-    console.log("Loaded suppliers: ", suppliers)
-    const products = await AppDataSource.manager.find(Product)
-    console.log("Loaded products: ", products)
+    console.log("Loading suppliers from the database...");
+    const suppliers = await AppDataSource.manager.find(Supplier);
+    console.log("Loaded suppliers: ", suppliers);
+    
+    console.log("Loading products from the database...");
+    const products = await AppDataSource.manager.find(Product);
+    console.log("Loaded products: ", products);
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    const app = express(),
+      port = process.env.PORT || 3000;
 
-    const app = express();
+    // const corsOptions = {
+    //     origin: 'http://unikits.be',
+    //     optionsSuccessStatus: 200
+    // }
 
-    const port = process.env.PORT ?? 3000;
+    // app.use(cors(corsOptions));
+
     app.listen(port, () => console.log("Server listening at port 3000"));
 
     app.get("/", (req, res) => {
-        res.send("Hello World");
+      res.send("Hello World");
     });
-}).catch(error => console.log(error))
+  })
+  .catch((error) => console.log(error));
